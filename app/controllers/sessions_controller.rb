8a7ce8612482c760aa1f0e464126dbe572a16741
tsuyoshi yamada <tuyoshi009922@gmail.com>
2018-07-15 20:15:28 +0000

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApplicationController
-  
-   def new
-   end
+
+  def new
+  end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      # エラーメッセージを作成する
-      flash.now[:danger] = 'Invalid email/password combination' 
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
